0d660bee6a11b700699874b0c7a8b9531052d616
Dar55 <dar55@yandex.ru>
2018-12-04 16:43:10 +0200

    1.2. Подключить сборщик Maven[#93516]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,14 +1,13 @@
 ﻿package ru.job4j.calculate;
-/** Класс Calculate
-*
-*
-*
+/** Calculate решение задачи
+* @author Валерий Васьков
+* @since 12.11.2018
+* @version 1
 */
 public class Calculate {
 /** Метод Main
-*
-*
-*
+* Конструктор, вывод строки в консоль
+* @param args - args
 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
