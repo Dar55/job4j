e610c51d5f76818cc734a917f1a48c949d4ef27f
Dar55 <dar55@yandex.ru>
2018-12-08 13:19:19 +0200

    4.1. Глупый бот.[#93480]

---
+++ chapter_001/src/test/java/ru/job4j/stupidBot/StupidBotTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.stupidBot;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+    /**
+     * @author Valeriy Vaskov (dar55@yandex.ru)
+     * @version $Id$
+     * @since 0.1
+     */
+    public class StupidBotTest {
+        @Test
+        public void whenGreetBot() {
+            StupidBot bot = new StupidBot();
+            assertThat(
+                    bot.answer("Привет, Бот."),
+                    is("Привет, умник.")
+            );
+        }
+
+        @Test
+        public void whenByuBot() {
+            StupidBot bot = new StupidBot();
+            assertThat(
+                    bot.answer("Пока."),
+                    is("До скорой встречи.")
+            );
+        }
+
+        @Test
+        public void whenUnknownBot() {
+            StupidBot bot = new StupidBot();
+            assertThat(
+                    bot.answer("Сколько будет 2 + 2?"),
+                    is("Это ставит меня в тупик. Спросите другой вопрос.")
+            );
+        }
+}
