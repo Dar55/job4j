298df009a0b025efc8c0ba18e35fa10b173aae1e
Dar55 <dar55@yandex.ru>
2018-12-18 11:19:30 +0200

    1. Реализация профессий в коде[#93570]

--- chapter_002/src/main/java/ru/job4j/professions/Doctor.java
+++ chapter_002/src/main/java/ru/job4j/professions/Doctor.java
@@ -1,9 +1,7 @@
 package ru.job4j.professions;
 
 public class Doctor extends Professions {
-    public String heal(Patient pacient) {
-        String diagnose;
-        diagnose = "a";
-        return diagnose;
+    public Diagnose heal(Patient pacient) {
+        return null;
     }
 }
