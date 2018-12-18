49ca7a512882689067c64e7df0ee1fd3f40dcf08
Dar55 <dar55@yandex.ru>
2018-12-18 15:35:23 +0200

    Дополнительное задание

--- chapter_001/src/main/java/ru/job4j/array/AdditionaTask.java
+++ chapter_001/src/main/java/ru/job4j/array/AdditionaTask.java
@@ -5,26 +5,27 @@ public class AdditionaTask {
     public int[] unionTwoArray(int[] array1, int[] array2) {
         int[]  array3 = new int[array1.length + array2.length];
         int s = 0;
-        int b = 0;
-        for (int i = 0; i <= array1.length; i++) {
-            for (int j = b; j < array2.length; j++) {
-                if (i == array1.length) {
-                    array3[s] = array2[j];
-                    s++;
-                }
-                else if (array1[i] <= array2[j]) {
-                    array3[s] = array1[i];
-                    s++;
-                    break;
+        int b;
+        int j = 0;
+        int i = 0;
+            while (i < array1.length && j < array2.length) {
+                if (array1[i] <= array2[j]) {
+                    array3[s++] = array1[i++];
                 } else {
-                    array3[s] = array2[j];
-                    b++;
-                    s++;
-                    i--;
-                    break;
+                    array3[s++] = array2[j++];
                 }
             }
+        if(i < array1.length) {
+            for (b = i; b < array1.length; b++) {
+                array3[s++] = array1[b];
+            }
         }
+         else if(j < array2.length) {
+                for (b = j; b < array2.length; b++){
+                    array3[s++] = array2[b];
+                }
+            }
+
         return array3;
     }
 }
--- chapter_001/src/test/java/ru/job4j/array/AdditionaTaskTest.java
+++ chapter_001/src/test/java/ru/job4j/array/AdditionaTaskTest.java
@@ -6,12 +6,30 @@ import static org.junit.Assert.assertThat;
 
 public class AdditionaTaskTest {
     @Test
-    public void equalTwoArray(){
+    public void whenSecondArrayMore(){
         int[] array1 = new int[] {1, 2, 3, 4, 5, 9};
         int[] array2 = new int[] {1, 4, 7, 8, 12, 21, 37, 44};
         AdditionaTask test = new AdditionaTask();
         int[] result = test.unionTwoArray(array1, array2);
         int[] expect = new int[] {1, 1, 2, 3, 4, 4, 5, 7, 8, 9, 12, 21, 37, 44};
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenFirstArrayMore(){
+        int[] array1 = new int[] {1, 2, 3, 4, 5, 9, 12, 17};
+        int[] array2 = new int[] {1, 4, 7, 8};
+        AdditionaTask test = new AdditionaTask();
+        int[] result = test.unionTwoArray(array1, array2);
+        int[] expect = new int[] {1, 1, 2, 3, 4, 4, 5, 7, 8, 9, 12, 17};
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenEqualArrays(){
+        int[] array1 = new int[] {1, 2, 3, 4};
+        int[] array2 = new int[] {1, 4, 7, 8};
+        AdditionaTask test = new AdditionaTask();
+        int[] result = test.unionTwoArray(array1, array2);
+        int[] expect = new int[] {1, 1, 2, 3, 4, 4, 7, 8};
         assertThat(result, is(expect));
     }
 }
