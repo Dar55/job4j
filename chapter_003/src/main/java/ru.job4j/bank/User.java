ed01e74f920ed7e2cb2201fb827a38d989b070c5
dar55 <dar55@yandex.ru>
2019-04-08 12:05:51 +0300

    3. ьанк

---
+++ chapter_003/src/main/java/ru.job4j/bank/User.java
@@ -0,0 +1,42 @@
+package ru.job4j.bank;
+
+public class User implements Comparable<User> {
+
+    private Integer id, age;
+    private String name, city;
+    User() {
+
+    }
+    User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(User o) {
+        return this.age.compareTo(o.age);
+    }
+
+    public void add(Integer id, String name, String city) {
+        this.id = id;
+        this.name = name;
+        this.city = city;
+    }
+    public int getAge() {
+        return age;
+    }
+    public int getId() {
+
+        return id;
+    }
+    public String getName() {
+
+        return name;
+    }
+    public String getCity() {
+
+        return city;
+    }
+
+
+}
